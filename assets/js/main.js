6f61166abee7e9584b97e679e66103c7c66345bf
Aquivido, Jean Paula <168424375+MisHersheys@users.noreply.github.com>
2024-10-17 11:52:07 +0000

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,41 @@
+/*Login Form*/
+const wrapper = document.querySelector('.wrapper');
+const loginlink = document.querySelector('.login-link');
+const registerlink = document.querySelector('.register-link');
+const btn = document.querySelector('.button.button--ghost');
+const iconclose = document.querySelector('.icon-close');
+const modalOverlay = document.querySelector('.modal-overlay'); // Get the modal overlay
+
+// Show the register form and modal
+registerlink.addEventListener('click', () => {
+  wrapper.classList.add('active');
+});
+
+// Switch back to the login form
+loginlink.addEventListener('click', () => {
+  wrapper.classList.remove('active');
+});
+
+// Open modal and show overlay
+btn.addEventListener('click', () => {
+  wrapper.classList.add('active-popup');
+  modalOverlay.classList.add('active'); // Show overlay
+});
+
+// Close modal and hide overlay
+iconclose.addEventListener('click', () => {
+  wrapper.classList.remove('active-popup');
+  modalOverlay.classList.remove('active'); // Hide overlay
+});
+
+// Close modal if overlay is clicked
+modalOverlay.addEventListener('click', (e) => {
+  if (e.target === modalOverlay) { // Make sure it's not a click inside the modal
+    wrapper.classList.remove('active-popup');
+    modalOverlay.classList.remove('active');
+  }
+});
+
 
 
 /*=============== SHOW MENU ===============*/
